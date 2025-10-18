73a0f32c140da067aa792f50a3535500c0d76ff4
gumraze-git <galaxydh4110@gmail.com>
2025-10-18 23:09:44 +0900

    refac: 사용자 repository 패키지 경로 및 사용하지 않는 메서드 제거 (#4)
    - `UserRepository`의 패키지 경로를 `user.repository`로 변경.
    - 사용되지 않는 메서드(`existsByEmail`, `findByEmail`) 제거.

--- src/main/java/org/ever/_4ever_be_auth/user/repository/UserRepository.java
+++ src/main/java/org/ever/_4ever_be_auth/user/repository/UserRepository.java
@@ -1,15 +1,13 @@
-package org.ever._4ever_be_auth.user;
+package org.ever._4ever_be_auth.user.repository;
 
+import org.ever._4ever_be_auth.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    boolean existsByEmail(String email);
 
     boolean existsByLoginEmail(String email);
-
-    Optional<User> findByEmail(String email);
 
     Optional<User> findByLoginEmail(String email);
 }
