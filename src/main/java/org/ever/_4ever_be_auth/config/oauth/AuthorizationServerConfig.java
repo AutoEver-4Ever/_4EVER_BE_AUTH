fc77718246b258008e744f9e652c5f7f757179ff
gumraze-git <galaxydh4110@gmail.com>
2025-10-18 15:03:18 +0900

    feat: spring security 설정 개선 (#2)
    - SecurityConfig 설정 추가로 API 접근 제어 설정 및 기본 인증 방식 설정.
    - AuthorizationServerConfig에 OIDC 및 클라이언트 등록 관련 설정 추가.

--- src/main/java/org/ever/_4ever_be_auth/config/oauth/AuthorizationServerConfig.java
+++ src/main/java/org/ever/_4ever_be_auth/config/oauth/AuthorizationServerConfig.java
@@ -5,8 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -24,16 +32,22 @@ public class AuthorizationServerConfig {
     // 메서드가 반환하는 객체를 스프링 컨테이너에 빈으로 등록하는 어노테이션
     @Bean
     @Order(1) // Bean 객체의 적용 순서를 지정하는 어노테이션
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http,
+                                                                      RegisteredClientRepository registeredClientRepository) throws Exception {
         // 인가 서버 표준 엔드포인트를 HttpSecurity에 붙일 때 필요한 세부 설정을 캡슐화 하기위한 객체
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
         // 생성한 configurer가 자동으로 노출할 엔드포인트의 경로 패턴을 RequestMatcher 형태로 받음
         RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
 
         http.securityMatcher(endpointsMatcher)
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/.well-known/openid-configuration",
+                                "/.well-known/jwks.json"
+                        ).permitAll().anyRequest().authenticated())
                 .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
-                .with(authorizationServerConfigurer, Customizer.withDefaults());
+                .with(authorizationServerConfigurer
+                        .oidc(Customizer.withDefaults()), Customizer.withDefaults());
 
         http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
@@ -50,5 +64,26 @@ public class AuthorizationServerConfig {
                 .authorizationEndpoint("/oauth2/authorize")
                 .tokenEndpoint("/oauth2/token")
                 .build();
+    }
+
+    // 인메모리 Registered Client 저장소 빈 정의 - 추후 DB 저장소로 교체 예정
+    // PKCE 전용 공개 클라이언트
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient erpWebClient = RegisteredClient.withId("erp-web-client")
+                .clientId("erp-web-client")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE) // PKCE 전용 public client
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://localhost:3000/oauth2/callback")
+                .scope(OidcScopes.OPENID)
+                .scope("erp.scm.read")
+                .tokenSettings(TokenSettings.builder().build())
+                .clientSettings(ClientSettings.builder()
+                        .requireProofKey(true)
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(erpWebClient);
     }
 }
